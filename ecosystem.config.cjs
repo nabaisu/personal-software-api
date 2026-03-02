bbd7da08332feeec21895338c7d6cc45e5d21020
Joao Nabais <35026036+nabaisu@users.noreply.github.com>
2026-03-02 20:30:44 +0000

    fix: use pm2 default log paths

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,7 +3,6 @@ module.exports = {
     {
       name: 'personal-software-backend',
       script: 'src/index.js',
-      instances: 1,
       exec_mode: 'fork',
       autorestart: true,
       watch: false,
@@ -11,9 +10,6 @@ module.exports = {
       env: {
         NODE_ENV: 'production',
       },
-      error_file: 'logs/err.log',
-      out_file: 'logs/out.log',
-      merge_logs: true,
       time: true,
     },
   ],
